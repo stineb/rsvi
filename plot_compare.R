2d114a3e25a1bb2db222353716d38daef4cd25bc
pchicote <pcasadei@agro.uba.ar>
2018-12-03 13:02:42 +0100

    scaling methods

--- plot_compare.R
+++ plot_compare.R
@@ -1,32 +1,51 @@
 plot_compare <- function(){
-    
-  dday_df <- data.frame(out_align$df_dday_aggbydday)
-  scaledata <- dday_df
-  ## Recognize value of dday 0
-  data0 <- dday_df[which(dday_df$dday==0),]
-  
-  ## Normalized by mean of dday 0
-  for(i in 2:length(scaledata)){
-    data0 <- dday_df[which(dday_df$dday==0), i]
-    scaledata[,i] <- scaledata[,i] / data0   
-  }
-  
   library(reshape2)
   library(ggplot2)
-  scaledata_melt <- melt(scaledata, id.vars = "dday")
-  var <- unique(scaledata_melt$variable)
   
-  ## Graphics
-  # Mean: var[7:12] or S meanvar[2:6]
-  mean <- var[7:12]
-  scalemean <- scaledata_melt[which(scaledata_melt$variable %in% mean),]
-  p <- ggplot(scalemean, aes(x=dday, y=value, col=variable)) +
-    geom_line(size=1) + geom_vline(xintercept=0) + theme_classic()
+  dday_df <- data.frame(out_align$df_dday_aggbydday)
+  scaledata <- dday_df
+  ## Variabiles: Median: var[8:13] or svar[2:7]
+  median <- colnames(scaledata)[8:13]
+  
+  ## Center in 0 dday: Recognize value of dday 0 
+  data0 <- scaledata[which(scaledata$dday==0),]
+  
+  ## 1) Substract value in dday 0 of all data points from each individual data point
+  center_scale <- scale(scaledata, center=data0, scale = FALSE)
+  # Graphics
+  scaledata_melt <- melt(data.frame(center_scale), id.vars = "dday")
+  scaledata_melt <- scaledata_melt[which(scaledata_melt$variable %in% median),]
+  psub <- ggplot(scaledata_melt, aes(x=dday, y=value, col=variable)) +
+    geom_line(size=1) + geom_vline(xintercept=0) + theme_classic() + labs(title="Substract")
+  
+  
+  ## 2) Quotient: Normalized by mean of dday 0
+  center_scaleq <- scaledata
+  for(i in 2:length(scaledata)){
+    center_scaleq[,i] <- scaledata[,i] / as.numeric(data0[i])   
+  }
+  
+  scaledata_melt <- melt(center_scaleq, id.vars = "dday")
+  scalemean <- scaledata_melt[which(scaledata_melt$variable %in% median),]
+  pq <- ggplot(scalemean, aes(x=dday, y=value, col=variable)) +
+    geom_line(size=1) + geom_vline(xintercept=0) + theme_classic() + labs(title="Quotient")
  
   psmooth <- ggplot(scalemean, aes(x=dday, y=value, col=variable)) +
     geom_point(size=1) + geom_vline(xintercept=0) + 
     geom_smooth(method=loess) + scale_y_continuous(limits=c(0,1.2)) +
     theme_classic()
+  
+  
+  ## 3) z-scoring: values in the transformed variable have the same relationship to one another as in the untransformed variable, but the transformed variable has mean 0 and standard deviation 1
+  # Subtract the value in dday 0 of all data points from each individual data point, then divide those points by the standard deviation of all point
+  center_scalez <- scale(scaledata, center=data0, scale = TRUE)
+  # Graphics
+  scaledata_melt <- melt(data.frame(center_scalez), id.vars = "dday")
+  scaledata_melt <- scaledata_melt[which(scaledata_melt$variable %in% median),]
+  pz <- ggplot(scaledata_melt, aes(x=dday, y=value, col=variable)) +
+    geom_line(size=1) + geom_vline(xintercept=0) + theme_classic() + labs(title="Z scoring")
+  # ggplot(scaledata_melt, aes(x=dday, y=value, col=variable)) +
+  #   geom_point(size=1) + geom_smooth() + geom_vline(xintercept=0) + theme_classic()
   
   # Q33-Q66
   # qq <- var[c(19:24,31:36)]
@@ -39,28 +58,8 @@ plot_compare <- function(){
   #       values=c("#F8766D","#B79F00","#00BA38","#00BFC4","#619CFF","#F564E3",
   #                "#F8766D","#B79F00","#00BA38","#00BFC4","#619CFF","#F564E3")) +
   #   scale_y_continuous(limits=c(0.5,1.2)) 
-  
-  ### Other ways to scale
-  # mean to subtract a constant from every value of a variable. center is FALSE, no centering is done
-  center_scale <- scale(scaledata, scale = FALSE)
-  # Center in 0: substract value in dday 0 of all data points from each individual data point
-  center_scale <- scale(scaledata, center=data0, scale = FALSE)
-  
-  # z-scoring: values in the transformed variable have the same relationship to one another as in the untransformed variable, but the transformed variable has mean 0 and standard deviation 1
-  # Subtract the mean of all data points from each individual data point, then divide those points by the standard deviation of all point
-  center_scale <- scale(scaledata, center=TRUE, scale = TRUE)
-  # Subtract the value in dday 0 of all data points from each individual data point, then divide those points by the standard deviation of all point
-  center_scale <- scale(scaledata, center=data0, scale = TRUE)
-  
-  # # Graphics
-  # scaledata_melt <- melt(data.frame(center_scale), id.vars = "dday")
-  # scaledata_melt <- scaledata_melt[which(scaledata_melt$variable %in% mean),]
-  # ggplot(scaledata_melt, aes(x=dday, y=value, col=variable)) +
-  #   geom_line(size=1) + geom_vline(xintercept=0) + theme_classic()
-  # ggplot(scaledata_melt, aes(x=dday, y=value, col=variable)) +
-  #   geom_point(size=1) + geom_smooth() + geom_vline(xintercept=0) + theme_classic()
 
-return(p)
+return(list(pq, pz))
 }
 
 
