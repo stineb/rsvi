70d7bd20443a68cafe4aa22e10bb4cfcbad41fe7
stineb <benjamin.stocker@gmail.com>
2019-07-26 18:44:37 -0700

    implemented model training new

--- for_randomForest_Categorical_bs.R
+++ for_randomForest_Categorical_bs.R
@@ -2,25 +2,25 @@ require( randomForest )
 require( caret )
 require( tidyverse )
 
-# Target
-nam_target <- "is_flue_drought"
-
 # load data and RF function
 load("data/ddf_v3_flue1.RData")
-source("wrap_random_forest.R")
+source("wrap_ml.R")
 
-# Data set
+# XXX Question: Can you share the script used for data cleaning?
+
+# Prepare data set
 ddf_rf <- ddf %>% 
   left_join(rename(obs_eval_NT$ddf, site=sitename), by=c("site", "date")) %>%
   mutate (PPFD = ppfd_fluxnet2015 * fapar) %>%  # xxx comment: what you call PPFD here is actually APAR
   filter(!is.na(flue)) %>%
-  select(site,nam_target, dovars, PPFD, temp, classid) %>% 
-  mutate(classid=factor(classid))  %>% 
+  select(site, is_flue_drought, flue, dovars, PPFD, temp, classid) %>% 
+  mutate(classid=factor(classid), is_flue_drought = factor(is_flue_drought))  %>% 
   drop_na()
   
-if (nam_target == "is_flue_drought"){
-  ddf_rf <- ddf_rf %>%  mutate(is_flue_drought = factor(is_flue_drought))
-}
+# XXX Test: Take only subset for testing
+subsites <- ddf_rf$site %>% unique() %>% head(4)
+ddf_sub <- ddf_rf %>% 
+  dplyr::filter(site %in% subsites)
 
 # Predictors combinations
 complete <- c("ndvi",    "evi",     "cci",     "pri",     "NIRv",    "PPFD", "temp", "classid")
@@ -31,6 +31,7 @@ tres <- c("ndvi",    "evi",     "cci",     "pri",     "NIRv",    "PPFD", "temp")
 # cinco <- c("ndvi",    "evi",  "PPFD", "temp", "classid")
 # seis <- c("pri",    "cci",  "PPFD", "temp", "classid")
 nueve <- c("ndvi",    "cci",  "temp", "classid")
+josep <- c("ndvi", "cci", "temp", "PPFD")
 
 # Each RSVI alone
 ndvi <- c("ndvi")
@@ -47,133 +48,194 @@ sitios <- c("AR-Vir", "AU-Ade", "AU-ASM", "AU-DaP", "AU-DaS", "AU-Dry", "AU-Stp"
             "US-SRG", "US-SRM", "US-WCr")
 
 
-#### LEAVE-ONE-OUT APPROACH
-
-# N? Drought dates per site
-# num_datos <- ddf_rf %>% 
-#   group_by(site, is_flue_drought) %>%
-#   filter(site %in% sitios) %>%
-#   summarise(no_rows = length(is_flue_drought)) 
-# num_datos <-  reshape2::dcast(data = num_datos,formula = site~is_flue_drought,fun.aggregate = sum,value.var = "no_rows")
-
-
 ##----------------------------------
-## Leave-group-out with all predictors
+## Classification
 ##----------------------------------
-rf_lgocv <- wrap_random_forest( df = ddf_rf, 
-                                nam_target = nam_target, 
-                                nam_instance = "site",
-                                train_method = "lgocv",
-                                predictors = predictores[[1]],  # use double square bracket here to access element of list
-                                tune = FALSE   
+## Leave-site-out with all predictors
+rf_mylgocv <- wrap_ml( df = ddf_sub, 
+                       nam_target = "is_flue_drought", 
+                       nam_group = "site",
+                       train_method = "myLGOCV",
+                       predictors = predictores[[1]],  # use double square bracket here to access element of list
+                       tune = FALSE,
+                       inner = TRUE
+)
+
+## "LGOCV" does the same thing as "myLGOCV"
+rf_lgocv <- wrap_ml( df = ddf_sub, 
+                     nam_target = "is_flue_drought", 
+                     nam_group = "site",
+                     train_method = "LGOCV",
+                     predictors = predictores[[1]],  # use double square bracket here to access element of list
+                     tune = FALSE,
+                     inner = TRUE
+)
+#save(rf_mylgocv, file = "./data/rf_mylgocv.Rdata")
+
+## Accuracy of the main model seems to be (almost) identical to 
+## the mean accuracy of the leave-site-out models. Makes sense.
+print(paste("Accuracy of main model:", rf_mylgocv$rf$results$Accuracy))
+print(paste("Mean accuracy across leave-site-out models:", purrr::map_dbl(rf_mylgocv$list_rf, "myresults") %>% mean()))
+
+print(paste("Accuracy of main model:", rf_lgocv$rf$results$Accuracy))
+print(paste("Mean accuracy across leave-site-out models:", purrr::map_dbl(rf_lgocv$list_rf, "myresults") %>% mean()))
+
+## Test: How is accuracy calculated?
+get_modobs <- function(df){
+  tibble(mod = as.vector(df$pred), obs = df$obs)
+}
+list_modobs_listmodels <- purrr::map(rf_mylgocv$list_rf, ~get_modobs(.))
+
+calc_performance <- function(df){
+  df_sum <- df %>% 
+    dplyr::mutate(good = obs==mod) %>% 
+    dplyr::summarise(good = sum(good))
+  df_sum$good / nrow(df)
+}
+acc_test <- purrr::map_dbl(list_modobs_listmodels, ~calc_performance(.)) %>% mean()  # that's the same as given above as 'Mean accuracy across leave-site-out models'
+print(paste("Re-calculated mean accuracy across leave-site-out models:", acc_test))
+
+# ## Question: 'rf_mylgocv$list_rf' is now a list of 'nsites' RF-models, while 'rf_mylgocv$rf' is a single model.
+# ## While the accuracy of the single model is given as rf_mylgocv$rf$results$Accuracy, when applying this model
+# ## for predicting at each site separately, we get an accuracy of 1 for each prediction. I don't understand 
+# ## how the single model relates to the list of models given by 'rf_mylgocv$list_rf'. This is shown here:
+# predict_bysite <- function(site, df, model, nam_target){
+#   df_test <- df %>% dplyr::filter(site == site)
+#   pred_test <- predict(model, newdata = df_test)
+#   out <- df_test %>% 
+#     dplyr::select(obs = {{nam_target}}) %>% 
+#     dplyr::mutate(mod = as.vector(pred_test))
+#   return(out)
+# }
+# df_test_listmodels <- purrr::map(as.list(sites), ~predict_bysite(., ddf_sub, rf_mylgocv$list_rf[[.]], nam_target))
+# df_test_globlmodel <- purrr::map(as.list(sites), ~predict_bysite(., ddf_sub, rf_mylgocv$rf, nam_target))
+# 
+# acc_listmodels <- purrr::map_dbl(df_test_listmodels, ~calc_performance(.)) %>% mean()
+# acc_globlmodel <- purrr::map_dbl(df_test_globlmodel, ~calc_performance(.)) %>% mean()
+# 
+# print(acc_listmodels)
+# print(acc_globlmodel)
+# ## --> I don't get that.
+
+## Construct confusion matrix
+cm_simple <- list_modobs_listmodels %>% bind_rows() %>% table()
+df_modobs_listmodels <- list_modobs_listmodels %>% bind_rows()
+cm <- confusionMatrix( data = as.factor(df_modobs_listmodels$mod), 
+                 reference = as.factor(df_modobs_listmodels$obs) )
+
+## Show results for each site (prediction trained at all other sites)
+vec_acc <- purrr::map_dbl(rf_mylgocv$list_rf, "myresults")
+df_acc <- tibble(site = sites, accuracy = vec_acc )
+print("Accuracy for each site (prediction trained at all other sites):")
+print(df_acc)
+
+## Get variable importance (see https://topepo.github.io/caret/variable-importance.html)
+var_imp <- varImp(rf_mylgocv$rf)
+list_var_imp <- purrr::map(rf_mylgocv$list_rf, ~varImp(.))
+
+## let's try now for the full dataset and josep's suggested predictors - for NN only because it's faster and better!!!
+rf_lgocv_class_full <- wrap_ml( df = ddf_rf, 
+                                nam_target = "is_flue_drought", 
+                                nam_group = "site",
+                                method = "nnet",
+                                train_method = "LGOCV",
+                                predictors = josep,  # use double square bracket here to access element of list
+                                tune = FALSE,
+                                inner = FALSE,
+                                classification = TRUE
+)
+print(rf_lgocv_class_full$rf$results) # Promising: has accuracy of 0.81
+
+## And the same using veg class (classid) instead of site as group
+rf_lgocv_class_full <- wrap_ml( df = ddf_rf, 
+                                nam_target = "is_flue_drought", 
+                                nam_group = "classid",
+                                method = "nnet",
+                                train_method = "LGOCV",
+                                predictors = josep,  # use double square bracket here to access element of list
+                                tune = FALSE,
+                                inner = FALSE,
+                                classification = TRUE
                                 )
+print(rf_lgocv_class_full$rf$results) # Has lower accuracy: 0.7526834
+
+## Recursive feature elimination (see https://topepo.github.io/caret/recursive-feature-elimination.html)
+## XXX doesn't work. Better do this "by hand": Look at how model performance changes for different sets of predictors.
+subsets <- complete %>% length() %>% seq() %>% rev()
+sites <- unique(df[[ "site" ]]) # 'nam_group' is site in this case (i.e. split the data by sparing data from one site for testing)
+group_folds <- vector(mode = "list", length = length(sites))
+for (i in seq_along(sites)) group_folds[[i]] <- which(ddf_sub[[nam_group]] != sites[i])
+names(group_folds) <- paste0("Subject", sites)
 
 
-resultados <- list()
-for (i in 1:length(sitios)){
+ctrl <- rfeControl(functions = lmFuncs,
+                   method = "LGOCV",
+                   index = group_folds,
+                   repeats = 5,
+                   verbose = FALSE
+                   )
 
-  test <- sitios[i]
+forml  <- as.formula(  paste( "is_flue_drought", "~", paste( complete, collapse=" + " ) ) )
 
-  ddf_train <- ddf_rf %>%
-    filter(site != test)
+set.seed(10)
+lmProfile <- caret::rfe( forml, 
+                         ddf_sub,
+                         sizes = subsets,
+                         rfeControl = ctrl
+                         )
 
-  ddf_test <- ddf_rf %>%
-    filter(site == test)
+##--------------------------------
+## Quantitative model
+##--------------------------------
+rf_lgocv_flue <- wrap_ml( df = ddf_sub, 
+                          method = "nnet",
+                          nam_target = "flue", 
+                          nam_group = "site",
+                          train_method = "LGOCV",
+                          predictors = josep,  # use double square bracket here to access element of list
+                          tune = FALSE,
+                          inner = TRUE,
+                          classification = FALSE
+                          )
 
-  rf_predict <- wrap_random_forest( train = ddf_train,
-                                    test = ddf_test,
-                                    target = nam_target,
-                                    predictors = predictores[[1]],  # use double square bracket here to access element of list
-                                    tune = TRUE
-                                    )
+print(paste("Results of main model:"))
+print(rf_lgocv_flue$rf$results)   # DIFFICULT: has r-squared of only 0.243
 
-  resultados[[i]] <- rf_predict
+# rf_lgocv_flue$rf$pred_test <- predict(rf_lgocv_flue$rf, newdata = ddf_sub, preProcess = pre_process ) ## yes, that's the same
+rf_lgocv_flue$rf$obs  <- ddf_sub[["flue"]]
+out_modobs <- tibble(mod = as.vector(rf_lgocv_flue$rf$pred), 
+                     obs = rf_lgocv_flue$rf$obs) %>%  
+  analyse_modobs2(mod = "mod", obs = "obs")
+out_modobs$gg
+
+## Evaluate "by hand":
+# Plot mod vs obs from all sub-models pooled (necessary to set 'inner' = TRUE to get all this data)
+get_modobs <- function(df){
+  tibble(mod = as.vector(df$pred), obs = df$obs)
 }
+list_modobs_listmodels <- purrr::map(rf_lgocv_flue$list_rf, ~get_modobs(.))
+out_modobs <- list_modobs_listmodels %>%
+  bind_rows %>% 
+  rbeni::analyse_modobs2(mod = "mod", obs = "obs")
+out_modobs$gg
 
+# this gives almost the same results
+results_by_listmodels <- purrr::map(list_modobs_listmodels, ~analyse_modobs2(.)) %>% 
+  purrr::map_dfr(., "results") %>% 
+  dplyr::summarise_all(.funs = mean)
 
-##----------------------------------
-## Complete Model
-## xxx comment: this seems to use an old 'wrap_random_forest()' functions. 'database' is not an argument in the function defined above.
-##              and 'Bestmtry' is not an element of the returned list.
-##----------------------------------
-models <- list()
-predictors <- predictores[[1]]
-models[[1]] <- wrap_random_forest(database = ddf_rf, target = nam_target, 
-                              predictors = predictors, sites = sitios, tune = TRUE)
+print(results_by_listmodels)
+print(rf_lgocv_flue$rf$results)
 
-#Save parameters tuned to apply in the others models
-mtry <- models[[1]]$Bestmtry
-  
-# ALL MODELS
-start <- Sys.time()
-for (p in 2:length(predictores)){
-  predictors <- predictores[[p]]
-  models[[p]] <- wrap_random_forest(database = ddf_rf, target= nam_target, predictors = predictors, sites=sitios, tune=FALSE)
-}
-end <- Sys.time()
-start-end
-
-# Global model: All predicted and all observed for each combinations of predictors
-Matrices <- list()
-for(m in 1:length(models)){
-  matrix <- confusionMatrix(table(unlist(modelos[[m]][3]), unlist(modelos[[m]][2])))
-  Matrices[[m]] <-matrix
-  # ov <- matrix$overall
-  # plot(t(matrix$table), main="Leave-one-out approach - ALL - nueve", xlab="Obs",ylab="Pred")
-}
-
-# # Compare predicted outcome and true outcome
-# modelo_final <- lm(formula = observados ~ predichos)
-# modelo_final$coefficients
-
-### BY VEGETATION CLASS ####
-clasification <- NULL
-resultados <- list()
-importancia <- list()
-resultados_clase <- list()
-
-for (c in 1:length(unique(ddf_rf$classid))){
-predichos <- NULL
-observados <- NULL
-sitios_clas <- unique(ddf_rf$site[which(ddf_rf$classid==unique(ddf_rf$classid)[c])])
-
-for (i in 1:length(sitios_clas)){
-  test <- sitios_clas[i]
-  
-  ddf_train <- ddf_rf %>%
-    filter(site != test) %>%
-    filter(classid == unique(ddf_rf$classid)[c])
-  
-  ddf_test <- ddf_rf %>%
-    filter(site == test) %>%
-    filter(classid == unique(ddf_rf$classid)[c])
-  
-  forml  <- as.formula(  paste( nam_target, "~", paste( predictors, collapse=" + " ) ) )
-  
-  rf <- randomForest(forml, data = ddf_train, preProc   = "range")
-  
-  if (length(ddf_test$is_flue_drought)!=0){
-    ddf_test <- rbind(ddf_train[1, ] , ddf_test)
-    ddf_test <- ddf_test [-1,]
-    
-    pred <- predict(model, newdata=ddf_test,  preProc   = "range")
-    
-    # CLASIFICACION
-    matrix <- confusionMatrix(table(pred, ddf_test$is_flue_drought))
-    ov <- matrix$overall
-    
-    clasification <- rbind(clasification,ov)
-    
-    predichos <- c(predichos, pred)
-    observados <- c(observados, ddf_test$is_flue_drought)
-    resultados[[i]] <- model
-    importancia[[i]] <- importance(model) 
-  }
-}  
-
-## CLASIFICACION
-matrix <- confusionMatrix(table(observados, predichos))
-resultados_clase[[c]] <- matrix
-# plot(t(matrix$table), main="Leave-one-out approach - ALL", xlab="Obs",ylab="Pred")
-}
+## knowing this, we can do the same for reduced predictors ('uno')
+rf_lgocv_flue2 <- wrap_ml( df = ddf_sub, 
+                           nam_target = "flue", 
+                           nam_group = "site",
+                           train_method = "LGOCV",
+                           predictors = uno,  # use double square bracket here to access element of list
+                           tune = FALSE,
+                           inner = FALSE,
+                           classification = FALSE
+)
+print(rf_lgocv_flue2$rf$results)
 
